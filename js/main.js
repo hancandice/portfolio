729d65f12b76a22fb91c2d3fee40fe56bcdb7f59
Candice Han <jeeyoung.han82@gmail.com>
2020-08-28 09:29:44 +0200

    Update with #home

--- js/main.js
+++ js/main.js
@@ -52,6 +52,11 @@ $(document).ready(function () {
   navbarFixed();
 });
 
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+// offsetTop - A Number, representing the top position of the element, in pixels
+
 // ********** set date ************
 
 const year = document.getElementById("year");
@@ -72,7 +77,7 @@ scrollLinks.forEach(function (link) {
     const element = document.getElementById(id);
     // Calculate the heights
     const navHeight = navbar.getBoundingClientRect().height;
-    console.log(navHeight);
+
     const containerHeight = linksContainer.getBoundingClientRect().height;
     const fixedNav = navbar.classList.contains("navbar_fixed");
 
